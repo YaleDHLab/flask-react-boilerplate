d49b61b7359531cd68c220300d65f5615fe98500
duhaime <douglas.duhaime@gmail.com>
2018-02-23 07:56:46 -0500

    remove redux dev tools to simplify

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware, connectRouter } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 import { createLogger } from 'redux-logger';
@@ -9,12 +9,12 @@ import { fetchItems } from './actions/items';
 import config from './config/client';
 
 const history = createBrowserHistory();
-const loggerMiddleware = createLogger()
+const loggerMiddleware = createLogger();
 
 let middlewares = [
   routerMiddleware(history),
-  thunkMiddleware
-]
+  thunkMiddleware,
+];
 
 // add the freeze dev middleware
 if (process.env.NODE_ENV !== 'production') {
@@ -25,25 +25,13 @@ if (process.env.NODE_ENV !== 'production') {
 // apply the middleware
 let middleware = applyMiddleware(...middlewares);
 
-// fetch compose enhancer
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// add the redux dev tools
-if (process.env.NODE_ENV !== 'production') {
-  if (window.devToolsExtension) {
-    middleware = compose(middleware, window.devToolsExtension())
-  } else if (composeEnhancer) {
-    middleware = composeEnhancer( applyMiddleware(middleware) )
-  }
-}
-
 // create the store
 const store = createStore(
   connectRouter(history)(rootReducer),
-  middleware
+  middleware,
 );
 
 // initialize app state
-store.dispatch(fetchItems(config.endpoint + 'items'))
+store.dispatch(fetchItems(config.endpoint + 'items'));
 
 export { store, history };
